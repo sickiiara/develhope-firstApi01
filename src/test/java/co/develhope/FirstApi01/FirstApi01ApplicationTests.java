56b528886ad1c8857dd71738c62f3966bc7e6d47
[Chiara] <chiara.galatro@gmail.com>
2022-07-30 16:27:59 +0200

    initializing project with Spring

---
+++ src/test/java/co/develhope/FirstApi01/FirstApi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.FirstApi01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApi01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
